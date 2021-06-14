db3c838b33c8385a234e0752906d1d4a29c7cca7
Gaetan Manchon <gmanchon@gmail.com>
2021-06-14 11:14:08 +0200

    add comments

--- content_script.js
+++ content_script.js
@@ -1,45 +1,57 @@
 
-// http://moto-occasion.motorevue.com/motos?particulier=0&professionnel=0&is_first_hand=0&is_guarantee=0&is_sale=0&search_ok=1
-
 window.onload = function() {
-    console.log("👉 👉 👉 👉 👉 👉 ")
-    make_prediction();
+
+  // run predictions for all of the ads
+  console.log("👉 👉 👉 make predictions")
+  make_prediction();
+  console.log("👍 👍 👍 done")
 }
 
 
 function make_prediction() {
-  document.querySelectorAll(".announces_list_item").forEach(
-    e => {
 
-      // retrieve element
-      mileage = e.getElementsByClassName("item_desc_km")[0].innerText.replace(' Km', '').replace(' ', '');
-      cylinders = e.getElementsByClassName("item_desc_cylindre")[0].innerText.replace(' cm3', '');
-      bike_year = e.getElementsByClassName("item_desc_millesime")[0].innerText.replace('Année ', '');
-      brand = e.querySelector('span[itemprop="brand"]').innerText.toLowerCase();
-      model = e.querySelector('span[itemprop="name"]').innerText.toLowerCase();
-      price = e.querySelector('span[itemprop="price"]').innerText.replace(' ', '');
-      id = e.querySelector('a.title_link.item_link').href.replace('.html', '').split('/').pop();
+  // select all of the ads
+  document.querySelectorAll(".announces_list_item").forEach(e => {
 
-      // print element content
-      console.log(mileage, cylinders, bike_year, brand, model, price, id);
+    // retrieve the elements of the ad
+    mileage = e.getElementsByClassName("item_desc_km")[0].innerText.replace(' Km', '').replace(' ', '');
+    cylinders = e.getElementsByClassName("item_desc_cylindre")[0].innerText.replace(' cm3', '');
+    bike_year = e.getElementsByClassName("item_desc_millesime")[0].innerText.replace('Année ', '');
+    brand = e.querySelector('span[itemprop="brand"]').innerText.toLowerCase();
+    model = e.querySelector('span[itemprop="name"]').innerText.toLowerCase();
+    price = e.querySelector('span[itemprop="price"]').innerText.replace(' ', '');
+    id = e.querySelector('a.title_link.item_link').href.replace('.html', '').split('/').pop();
 
-      // api request
-      url = `http://localhost:8000/predict?mileage=${mileage}&cylinders=${cylinders}&bike_year=${bike_year}&brand=${brand}&model=${model}&price=${price}&id=${id}`
-      console.log(url)
-      fetch(url).then(response => {
-        if (response.ok === true) {
-          return response.json();
-        }
-        throw new Error('Request failed!');
-      }, networkError => {
-        console.log(networkError.message);
-      }).then(jsonResponse => {
-        console.log(jsonResponse.price);
-        console.log("🧙‍♂️ 🧙‍♂️ 🧙‍♂️ 🧙‍♂️");
+    // print ad content
+    console.log(mileage, cylinders, bike_year, brand, model, price, id);
 
-        e.querySelector(".item_price").insertAdjacentHTML("beforeend", `<li>🔥 ${jsonResponse.price} 🔥</li>`);
+    // build api request
+    url = `http://localhost:8000/predict?mileage=${mileage}&cylinders=${cylinders}&bike_year=${bike_year}&brand=${brand}&model=${model}&price=${price}&id=${id}`;
+    console.log(url);
 
-      });
-    }
-  );
+    // retrieve prediction from api
+    fetch(url).then(response => {
+
+      // check whether request is ok
+      if (response.ok === true) {
+        return response.json();
+      }
+
+      // log request error
+      console.log(response);
+      throw new Error('Request failed!');
+    }, networkError => {
+
+      // log network error
+      console.log(response);
+      console.log(networkError.message);
+    }).then(jsonResponse => {
+
+      // retrieve response price
+      price = jsonResponse.price
+
+      // inject content
+      e.querySelector(".item_price").insertAdjacentHTML("beforeend", `<li>🔥 ${jsonResponse.price} 🔥</li>`);
+    });
+  });
 }
